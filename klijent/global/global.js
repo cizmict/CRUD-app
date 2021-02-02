15ba141589e6a36f210acde08fb1d990370f6d33
cizmic <tika.cizmic@gmail.com>
2021-02-02 17:14:43 +0100

    promenjen kod za datum

--- klijent/global/global.js
+++ klijent/global/global.js
@@ -1,5 +1,5 @@
 function formatDate(jsonDate) {
-  const date = new Date(JSON.parse(`"${jsonDate}"`));
+  const date = new Date(jsonDate);
   const d = date.getDate();
   const m = date.getMonth() + 1; //Month from 0 to 11
   const y = date.getFullYear();
